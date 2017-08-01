ce2a4b1fe9dfdba3dc06c8fb5498bfe55b23ea43
Daniel McBryde <mcbryded@gmail.com>
2017-08-01 12:11:50 -0500

    added more complexity to the test suite, and minor changes to the front end of the home page

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 
-class Home extends Component {    
-  
+class Home extends Component {
+
   handleSubmit = (e) => {
     e.preventDefault();
     let teacherName = this.name.value;
     let teacherTopic = this.topic.value;
     let path = `teachers/${teacherTopic}/${teacherName}`;
-    this.props.history.push(path); 
+    this.props.history.push(path);
   }
-  
+
   render() {
     return (
       <div className="main-content home">
-        <h2>Front End Course Directory</h2>
-        <p>This fun directory is a project for the <em>React Router Basics</em> course on Treehouse.</p>
-        <p>Learn front end web development and much more! This simple directory app offers a preview of our course library. Choose from many hours of content, from HTML to CSS to JavaScript. Learn to code and get the skills you need to launch a new career in front end web development.</p>
-        <p>We have thousands of videos created by expert teachers on web design and front end development. Our library is continually refreshed with the latest on web technology so you will never fall behind.</p>
+        <h2>React app and Automated testing playground</h2>
+        <p>This fun project is where I'm playing with a react project I cloned from team treehouse, and using it to practice automated testing with selenium and mocha with chai.</p>
+        <p>I haven't made many changes to the react app, as I'm mostly just creating the test suite right now, and exploring what it can do.</p>
+        <p>If you happen to check this site out and have automation advice feel free to let me know.</p>
         <hr />
-        <h3>Featured Teachers</h3>
+        <h3>The Teacher Search Form</h3>
         <form onSubmit={this.handleSubmit}>
           <input type="text" placeholder="Name" ref={ (input) => this.name = input } />
           <input type="text" placeholder="Topic" ref={ (input) => this.topic = input } />
@@ -30,6 +30,3 @@ class Home extends Component {
 }
 
 export default Home;
-
-
-
