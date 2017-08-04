b977f364aa1913a01f1b7bfb832b29f1906822a4
Daniel McBryde <mcbryded@gmail.com>
2017-08-04 15:36:45 -0500

    Added tests for different searches, includeing those that leave one or the other of the inputs blank to check behavior of non-valid inputs.  Input validation needed.

--- testing/searchTest.js
+++ testing/searchTest.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect;
 describe('Are elements visible', function() {
   describe('Visibility', function() {
-    it('should show that all elements are visible with true', function() {
+    it('should show correct page when searching for teacher', function() {
       browser.url('http://localhost:3000/');
       //Fill in Input for Name
       browser.element('//*[@id="root"]/div/div/form/input[1]').setValue('Angie McAngular');
@@ -13,6 +13,54 @@ describe('Are elements visible', function() {
       expect(browser.getUrl('http://localhost:3000/teachers/Angular/Angie%20McAngular'));
 
     });
+
+    it('should show correct page when searching for teacher', function() {
+      browser.url('http://localhost:3000/');
+      //Fill in Input for Name
+      browser.element('//*[@id="root"]/div/div/form/input[1]').setValue('NodeStradamus');
+      //Fill in Input for Subject
+      browser.element('//*[@id="root"]/div/div/form/input[2]').setValue('Node');
+      //Click on Submit button
+      browser.click('#root > div > div > form > button');
+      //Check Resulting Url is course-directory
+      expect(browser.getUrl('http://localhost:3000/teachers/Node/NodeStradamus'));
+
+    });
+
+
+    it('should show not found page if name left blank', function() {
+      browser.url('http://localhost:3000/');
+      //Fill in Input for Name
+      browser.element('//*[@id="root"]/div/div/form/input[1]').setValue('');
+      //Fill in Input for Subject
+      browser.element('//*[@id="root"]/div/div/form/input[2]').setValue('Node');
+      //Click on Submit button
+      browser.click('#root > div > div > form > button');
+      //Check That Resulting Page is the "Page Not Found" component
+      expect(browser.isVisible('not-found'));
+      //Check Resulting Url is course-directory
+      expect(browser.getUrl('http://localhost:3000/teachers/Node/'));
+
+    });
+
+
+    it('should show not found page if topic left blank', function() {
+      browser.url('http://localhost:3000/');
+      //Fill in Input for Name
+      browser.element('//*[@id="root"]/div/div/form/input[1]').setValue('NodeStradamus');
+      //Fill in Input for Subject
+      browser.element('//*[@id="root"]/div/div/form/input[2]').setValue('');
+      //Click on Submit button
+      browser.click('#root > div > div > form > button');
+      //Check That Resulting Page has the "Not-Found" selector
+      expect(browser.isVisible('not-found'));
+      //Check Resulting Url is course-directory
+      expect(browser.getUrl('http://localhost:3000/teachers//NodeStradamus'));
+
+    });
+
+
+
 });
 
 
